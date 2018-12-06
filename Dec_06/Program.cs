3fa837f430428ddf970dc10cfb38ef4ce286727c
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2018-12-06 09:35:00 +0100

    Dag 6 løsning

---
+++ Dec_06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Dec_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+
+            Console.WriteLine(Enumerable
+                .Range(1, 18163106)
+                .Where(num =>
+                {
+                    var digits = num.ToString();
+                    var numzerodigits = digits.Count(c => c == '0');
+                    return numzerodigits > (digits.Length - numzerodigits);
+                })
+                .Select(i => (long) i)
+                .Sum());
+
+            stopWatch.Stop();
+            var x = stopWatch.Elapsed;
+        }
+    }
+}
