cb07f3b765cbca7748aecfac883a75785d41463e
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2018-12-06 07:21:13 +0100

    working

--- Dec_05/Program.cs
+++ Dec_05/Program.cs
@@ -1,39 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Xml.Schema;
 
 namespace Dec_05
 {
-    enum Op { add, sub, concat }
-
-
-    class Program
+    internal enum Op
     {
-        static readonly int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1 };
+        add,
+        sub,
+        concat
+    }
+
+
+    internal class Program
+    {
+        private static readonly int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1};
         //static readonly int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
 
 
         private static bool Is42(IList<int> numbers, IList<Op> ops)
         {
-            var texts = new List<string>();
-
             long result = 0;
-            bool starting = true;
+            var starting = true;
 
 
-            for (int i = 0; i < numbers.Count; i++)
+            for (var i = 0; i < numbers.Count; i++)
             {
-                string number = "";
-                int j = i;
+                var number = "";
+                var j = i;
                 number += numbers[j].ToString();
-                while (j < ops.Count && ops[j] == Op.concat )
+                while (j < ops.Count && ops[j] == Op.concat)
                 {
                     j++;
                     number += numbers[j].ToString();
                 }
 
-                long num = long.Parse(number);
-
+                var num = long.Parse(number);
 
 
                 if (starting)
@@ -52,10 +53,8 @@ namespace Dec_05
                             result -= num;
                             break;
                         case Op.concat:
-                            throw      new Exception();
-                            
+                            throw new Exception();
                     }
-
                 }
 
                 i = j;
@@ -65,7 +64,6 @@ namespace Dec_05
         }
 
 
-
         private static IEnumerable<IList<T>> Permutations<T>(IList<T> objects, int dimensions)
         {
             foreach (var o in objects)
@@ -73,39 +71,31 @@ namespace Dec_05
                 var result = new T[dimensions];
                 result[0] = o;
                 if (dimensions == 1)
-                {
                     yield return result;
-                }
                 else
                     foreach (var sublist in Permutations(objects, dimensions - 1))
                     {
-                        for (int i = 1; i < dimensions; i++)
-                        {
-                            result[i] = sublist[i - 1];
-                        }
+                        for (var i = 1; i < dimensions; i++) result[i] = sublist[i - 1];
                         yield return result;
                     }
             }
         }
 
 
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            List<Op> ops = new List<Op> { Op.add, Op.sub, Op.concat };
+            var ops = new List<Op> {Op.add, Op.sub, Op.concat};
 
-            int count = 0;
+            var count = 0;
             var q = Permutations(ops, numbers.Length - 1);
             foreach (var p in q)
-            {
                 if (Is42(numbers, p))
                 {
                     Console.WriteLine(p);
                     count++;
                 }
-            }
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(count);
         }
     }
 }
